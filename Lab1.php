db171f41f0927f2568a7bf7878337b0b9ac7b8f7
Shyleen19 <132396480+Shyleen19@users.noreply.github.com>
2024-10-13 20:19:06 +0300

    Lab1 task

---
+++ Lab1.php
@@ -0,0 +1,10 @@
+?>
+$regNumber = "CIT-223-078/2022";
+$courseName = "Computer Science";
+
+echo "<h1>Student Information</h1>";
+  echo "<p>Registration Number: $regNumber</p>";
+  echo "<p>Course Name: $courseName</p>"; echo "<h1>Student Information</h1>";
+  echo "<p>Registration Number: $regNumber</p>";
+  echo "<p>Course Name: $courseName</p>";
+?>
