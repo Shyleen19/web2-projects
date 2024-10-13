49ceca6120b3389dac33fc9cdc0e232a3180771b
Shyleen19 <132396480+Shyleen19@users.noreply.github.com>
2024-10-13 21:06:35 +0300

    Lab 3.2 Task

---
+++ lab 3.2 .php
@@ -0,0 +1,12 @@
+?>
+  $grNo = "CIT-223-078/2022";
+  $name = "Shyleen Mwendwa";
+
+  // Display G.R No and Name using echo
+  echo "<p>G.R No: " . $grNo . "</p>";
+  echo "<p>Name: " . $name . "</p>";
+
+  // Display G.R No and Name using print
+  print "<p>G.R No: " . $grNo . "</p>";
+  print "<p>Name: " . $name . "</p>";
+?>
