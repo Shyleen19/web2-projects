ab8109d0725bbf8dc537ed336ca968c3d037417e
Shyleen19 <132396480+Shyleen19@users.noreply.github.com>
2024-10-13 20:41:54 +0300

    Lab 2 Task

---
+++ lab 2.php
@@ -0,0 +1,11 @@
+?>
+$name = "M";
+  $age = 21;
+  $gender = "Female";
+  $nationality = "Kenyan";
+  echo "<h1>My data</h1>";
+  echo "<p>Name: $name</p>";
+  echo "<p>Age: $age</p>";
+  echo "<p>Gender: $gender</p>";
+  echo "<p>Nationality: $nationality</p>";
+  ?>
