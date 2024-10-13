86b279b1096a2d068116939ba7fdb20b9a7b2458
Shyleen19 <132396480+Shyleen19@users.noreply.github.com>
2024-10-13 21:01:31 +0300

    Lab 3.1 Task

---
+++ lab 3.1.php
@@ -0,0 +1,11 @@
+?>
+  $employeeCode = "001";
+  $employeeName = "ABC";
+  $employeeSalary = 25000;
+  $workingHours = 7.5;
+
+  echo "<p>Employee Code: " . $employeeCode . "</p>";
+  echo "<p>Employee Name: " . $employeeName . "</p>";
+  echo "<p>Employee Salary: " . $employeeSalary . "</p>";
+  echo "<p>Working Hours: " . $workingHours . "</p>";
+?>
