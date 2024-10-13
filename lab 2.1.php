2a746f3561ace898ca3b45bee7f88427263e11ff
Shyleen19 <132396480+Shyleen19@users.noreply.github.com>
2024-10-13 20:43:13 +0300

    Lab 2.1 task

---
+++ lab 2.1.php
@@ -0,0 +1,10 @@
+?>
+  echo "<h1>Welcome to My Website</h1>";
+  echo "<p>This is an <b>Eccommerce website</b> selling ankara outfits </p>";
+  echo "<br>";
+  echo "<h2>Physical Location</h2>";
+  echo "<p>We are located at <font color='blue'>Maasai Mall - rongai</font> Visit us today</p>";
+  echo "<br>";
+  echo "<h3>contact us today</h3>";
+  echo "<p>you can reach us our email address <font size='4'>below for any inquiries</font>.</p>";
+?>
