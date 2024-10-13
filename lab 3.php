b4bebbd971b2fc7ba125a4f91ecb5e96dcc7703a
Shyleen19 <132396480+Shyleen19@users.noreply.github.com>
2024-10-13 21:00:15 +0300

    Lab 3 Task

---
+++ lab 3.php
@@ -0,0 +1,12 @@
+?>
+  define("EMPLOYEE_CODE", "001");
+  define("EMPLOYEE_NAME", "ABC");
+  define("EMPLOYEE_SALARY", 25000);
+  define("WORKING_HOURS", 7.5);
+
+  // Display employee information
+  echo "<p>Employee Code: " . EMPLOYEE_CODE . "</p>";
+  echo "<p>Employee Name: " . EMPLOYEE_NAME . "</p>";
+  echo "<p>Employee Salary: " . EMPLOYEE_SALARY . "</p>";
+  echo "<p>Working Hours: " . WORKING_HOURS . "</p>";
+?>
